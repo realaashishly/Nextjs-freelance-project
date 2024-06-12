547d27108ea9d70b48463136ac5adce60059b65f
Aashish Singh <11aashish.singh@gmail.com>
2024-06-12 09:22:05 +0530

    added verifySchema and signupSchema

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -2,6 +2,15 @@ import { z } from "zod";
 
 export const usernameValidation = z
     .string()
-    .min()
-    .max()
+    .min(3, "Username must be atleast 3 characters")
+    .max(20, "Username must be less than 20 characters")
+    .regex(/^[a-zA-Z0-9_]+$/, "Username must be alphanumeric");
 
+export const signUpSchema = z.object({
+    username: usernameValidation,
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z
+        .string()
+        .min(8, { message: "Password must be atleast 8 characters" })
+        .max(16, { message: "Password must be less than 16 characters" }),
+});
